c11954c2b23d41c56389e29788c1c6268781e2ea
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-08 15:58:24 +0400

    xpo

--- CS/XPO/UseTreeList/UseTreeList.Module/DatabaseUpdate/Updater.cs
+++ CS/XPO/UseTreeList/UseTreeList.Module/DatabaseUpdate/Updater.cs
@@ -5,29 +5,79 @@ using DevExpress.ExpressApp.Updating;
 using DevExpress.Xpo;
 using DevExpress.ExpressApp.Xpo;
 using DevExpress.Persistent.BaseImpl;
+using HowToUseTreeListEditor.Module;
 
 namespace UseTreeList.Module.DatabaseUpdate;
 
 // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Updating.ModuleUpdater
 public class Updater : ModuleUpdater {
-    public Updater(IObjectSpace objectSpace, Version currentDBVersion) :
-        base(objectSpace, currentDBVersion) {
-    }
+    public Updater(IObjectSpace objectSpace, Version currentDBVersion) : base(objectSpace, currentDBVersion) { }
     public override void UpdateDatabaseAfterUpdateSchema() {
-        base.UpdateDatabaseAfterUpdateSchema();
-        //string name = "MyName";
-        //DomainObject1 theObject = ObjectSpace.FirstOrDefault<DomainObject1>(u => u.Name == name);
-        //if(theObject == null) {
-        //    theObject = ObjectSpace.CreateObject<DomainObject1>();
-        //    theObject.Name = name;
-        //}
+        for (int k = 0; k < 3; k++) {
+            string projectGroupNameWI = "ProjectGroupWithIssue" + k;
+            string projectGroupName = "ProjectGroup" + k;
 
-		//ObjectSpace.CommitChanges(); //Uncomment this line to persist created object(s).
+            ProjectGroupWithIssues projectGroupWithIssue = CreateObject<ProjectGroupWithIssues>("Name", projectGroupNameWI);
+            ProjectGroup projectGroup = CreateObject<ProjectGroup>("Name", projectGroupName);
+
+            projectGroupWithIssue.Name = projectGroupNameWI;
+            projectGroup.Name = projectGroupName;
+
+            PopulateWithIssue(projectGroupWithIssue);
+
+            for (int i = 0; i < 3; i++) {
+                string projectNameWI = "ProjectWithIssue" + k + " - " + i;
+                string projectName = "Project" + k + " - " + i;
+
+                ProjectWithIssues projectWithIssue = CreateObject<ProjectWithIssues>("Name", projectNameWI);
+                Project project = CreateObject<Project>("Name", projectName);
+
+                projectWithIssue.Name = projectNameWI;
+                project.Name = projectName;
+
+                PopulateWithIssue(projectWithIssue);
+
+                for (int j = 0; j < 3; j++) {
+                    string projectAreaNameWI = "ProjectAreaWithIssue" + k + " - " + i + " - " + j;
+                    string projectAreaName = "ProjectArea" + k + " - " + i + " - " + j;
+
+                    ProjectAreaWithIssues projectAreaWI = CreateObject<ProjectAreaWithIssues>("Name", projectAreaNameWI);
+                    ProjectArea projectArea = CreateObject<ProjectArea>("Name", projectAreaName);
+
+                    projectAreaWI.Name = projectAreaNameWI;
+                    projectArea.Name = projectAreaName;
+
+                    PopulateWithIssue(projectAreaWI);
+
+                    projectWithIssue.ProjectAreasWithIssues.Add(projectAreaWI);
+                    project.ProjectAreas.Add(projectArea);
+                }
+                projectGroupWithIssue.ProjectsWithIssues.Add(projectWithIssue);
+                projectGroup.Projects.Add(project);
+            }
+        }
+        ObjectSpace.CommitChanges();
+
+        base.UpdateDatabaseAfterUpdateSchema();
     }
-    public override void UpdateDatabaseBeforeUpdateSchema() {
-        base.UpdateDatabaseBeforeUpdateSchema();
-        //if(CurrentDBVersion < new Version("1.1.0.0") && CurrentDBVersion > new Version("0.0.0.0")) {
-        //    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
-        //}
+
+    void PopulateWithIssue(CategoryWithIssues category) {
+        for (int l = 0; l < 3; l++) {
+            string issueName = category.Name + " -" + "Issue" + l;
+            Issue issue = CreateObject<Issue>("Subject", issueName);
+            issue.Subject = issueName;
+            category.Issues.Add(issue);
+        }
     }
+
+    T CreateObject<T>(string propertyName, string value) {
+
+        T theObject = ObjectSpace.FindObject<T>(new OperandProperty(propertyName) == value);
+        if (theObject == null)
+            theObject = ObjectSpace.CreateObject<T>();
+
+        return theObject;
+
+    }
+
 }
